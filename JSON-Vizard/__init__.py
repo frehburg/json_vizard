2b77479707c4085ee17c71fc78d76678876f68ee
frehburg <43171336+frehburg@users.noreply.github.com>
2023-08-29 15:26:42 +0200

    Boilerplate hashing (#16)
    * Wrote boilerplate for hashing
    
    * Added hashing to __init__.py

--- JSON-Vizard/__init__.py
+++ JSON-Vizard/__init__.py
@@ -1,4 +1,9 @@
 from .FileHandling import read, write
 from .Enums import ReturnType, FileType
+from .Hashing import unique_hash, compare_hashes
 
-__all__ = ['read', 'write', 'ReturnType', 'FileType']
+__all__ = [
+    'read', 'write',
+    'ReturnType', 'FileType',
+    'unique_hash', 'compare_hashes'
+]
