62ba372615fde89e6beb8ed834ef981b585c7e03
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-29 16:47:48 +0200

    created tests for removing arg

---
+++ test/test_ArgumentManipulation.py
@@ -0,0 +1,61 @@
+import pytest
+
+from json_vizard.ArgumentManipulation import remove_arg
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_kwargs_key():
+    dict1 = {
+        "name": "John Johnson",
+        "age": 99,
+        "location": {
+            "home": "Testville",
+            "work": "Testington"
+        }
+    }
+    remove_arg(dict1, key="name")
+    assert dict1 == {
+        "age": 99,
+        "location": {
+            "home": "Testville",
+            "work": "Testington"
+        }
+    }
+
+
+def test_kwargs_keys():
+    dict1 = {
+        "name": "John Johnson",
+        "age": 99,
+        "location": {
+            "home": "Testville",
+            "work": "Testington"
+        }
+    }
+    remove_arg(dict1, keys=["location", "home"])
+    assert dict1 == {
+        "name": "John Johnson",
+        "age": 99,
+        "location": {
+            "work": "Testington"
+        }
+    }
+
+
+def test_args():
+    dict1 = {
+        "name": "John Johnson",
+        "age": 99,
+        "location": {
+            "home": "Testville",
+            "work": "Testington"
+        }
+    }
+    remove_arg(dict1, "location", "home")
+    assert dict1 == {
+        "name": "John Johnson",
+        "age": 99,
+        "location": {
+            "work": "Testington"
+        }
+    }
