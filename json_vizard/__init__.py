c83f10b7572e60ac87daf618063ca10d582ca006
frehburg <43171336+frehburg@users.noreply.github.com>
2023-08-29 16:40:52 +0200

    Boilerplate remove arg (#25)
    * Added remove_arg boilerplate
    
    * added to init

--- json_vizard/__init__.py
+++ json_vizard/__init__.py
@@ -2,10 +2,12 @@ from .FileHandling import read, write
 from .Enums import ReturnType, FileType
 from .Hashing import unique_hash, compare_hashes
 from .Indexing import get_from
+from .ArgumentManipulation import remove_arg
 
 __all__ = [
     'read', 'write',
     'ReturnType', 'FileType',
     'unique_hash', 'compare_hashes',
-    'get_from'
+    'get_from',
+    'remove_arg'
 ]
