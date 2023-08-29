f4f280c5ed1a2191f7cd4aa8bbfdd663ef484681
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-29 16:07:07 +0200

    Added indexing to init

--- json_vizard/__init__.py
+++ json_vizard/__init__.py
@@ -1,9 +1,11 @@
 from .FileHandling import read, write
 from .Enums import ReturnType, FileType
 from .Hashing import unique_hash, compare_hashes
+from .Indexing import get_from
 
 __all__ = [
     'read', 'write',
     'ReturnType', 'FileType',
-    'unique_hash', 'compare_hashes'
+    'unique_hash', 'compare_hashes',
+    'get_from'
 ]
