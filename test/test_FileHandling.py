2266878190bb2bd835fb734ee9d6ee4f70f6c039
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-29 16:56:06 +0200

    defined all methods for read testing and skipped them

---
+++ test/test_FileHandling.py
@@ -0,0 +1,91 @@
+import pytest
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_deeply_nested():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_deeply_nested():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_deeply_nested():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_empty_line_end_of_file():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_empty_line_end_of_file():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_empty_line_end_of_file():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_empty_line_in_between():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_empty_line_in_between():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_empty_line_in_between():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_lists():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_lists():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_lists():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_mixed_types():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_mixed_types():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_mixed_types():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_json_numbers():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_bson_numbers():
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_read_txt_numbers():
+    pass
