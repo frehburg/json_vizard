1a89b41a7595fc337f1d423055417573ad5bf82d
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-30 09:38:41 +0200

    Added search to init.py

--- json_vizard/__init__.py
+++ json_vizard/__init__.py
@@ -1,13 +1,13 @@
 from .FileHandling import read, write, remove
 from .Enums import ReturnType, FileType
 from .Hashing import unique_hash, compare_hashes
-from .InformationRetrieval import get_from
+from .InformationRetrieval import get_from, search
 from .ArgumentManipulation import add_arg, remove_arg, modify_arg
 
 __all__ = [
     'read', 'write', 'remove',
     'ReturnType', 'FileType',
     'unique_hash', 'compare_hashes',
-    'get_from',
+    'get_from', 'search',
     'add_arg', 'remove_arg', 'modify_arg'
 ]
