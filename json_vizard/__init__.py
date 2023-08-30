fa95ada000e898c11f1b99e14b84fffb36b5d7de
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-30 09:00:40 +0200

    Added add arg to init

--- json_vizard/__init__.py
+++ json_vizard/__init__.py
@@ -2,12 +2,12 @@ from .FileHandling import read, write, remove
 from .Enums import ReturnType, FileType
 from .Hashing import unique_hash, compare_hashes
 from .InformationRetrieval import get_from
-from .ArgumentManipulation import remove_arg, modify_arg
+from .ArgumentManipulation import add_arg, remove_arg, modify_arg
 
 __all__ = [
     'read', 'write', 'remove',
     'ReturnType', 'FileType',
     'unique_hash', 'compare_hashes',
     'get_from',
-    'remove_arg', 'modify_arg'
+    'add_arg', 'remove_arg', 'modify_arg'
 ]
