e3af11dfbee5f179755c19fddf313b166410d90c
frehburg <43171336+frehburg@users.noreply.github.com>
2023-08-30 09:09:44 +0200

    Added tests for add args (#46)

--- test/test_ArgumentManipulation.py
+++ test/test_ArgumentManipulation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from json_vizard.ArgumentManipulation import remove_arg, modify_arg
+from json_vizard import remove_arg, modify_arg, add_arg
 
 
 @pytest.mark.skip(reason='Not implemented yet')
@@ -241,4 +241,110 @@ def test_modify_list():
         "age": 99,
         "is_human": True,
         "location": ["Test City", "Test Town"]
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_int():
+    dict1 = {
+        "name": "John Johnson",
+    }
+    add_arg(dict1, 99, "age")
+    assert dict1 == {
+        "name": "John Johnson",
+        "age": 99
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_float():
+    dict1 = {
+        "name": "John Johnson",
+    }
+    add_arg(dict1, 99.1, "age")
+    assert dict1 == {
+        "name": "John Johnson",
+        "age": 99.1
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_bool():
+    dict1 = {
+        "name": "John Johnson",
+    }
+    add_arg(dict1, True, "is_human")
+    assert dict1 == {
+        "name": "John Johnson",
+        "is_human": True
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_dict():
+    dict1 = {
+        "name": "John Johnson",
+    }
+    locations = {
+        "dwellings": {
+            "primary": "Test City",
+            "secondary": "Test Town"
+        },
+        "work": "Testington"
+    }
+    add_arg(dict1, new_val=locations, key="location")
+    assert dict1 == {
+        "name": "John Johnson",
+        "location": {
+            "dwellings": {
+                "primary": "Test City",
+                "secondary": "Test Town"
+            },
+            "work": "Testington"
+        }
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_list():
+    dict1 = {
+        "name": "John Johnson",
+    }
+    locations = ["Test City", "Test Town"]
+    add_arg(dict1, new_val=locations, key="location")
+    assert dict1 == {
+        "name": "John Johnson",
+        "location": ["Test City", "Test Town"]
+    }
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_add_deep():
+    dict1 = {
+        "name": "John Johnson",
+        "location": {
+            "private": {
+                "vacation_homes" : {
+                    "beach": "Test Beach",
+                }
+            }
+        }
+    }
+
+    add_arg(
+        dict1,
+        new_val="Mount Testmore",
+        keys=["location", "private", "vacation_homes", "mountain"]
+    )
+
+    assert dict1 == {
+        "name": "John Johnson",
+        "location": {
+            "private": {
+                "vacation_homes" : {
+                    "beach": "Test Beach",
+                    "mountain": "Mount Testmore"
+                }
+            }
+        }
     }
